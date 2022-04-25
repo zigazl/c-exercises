a4d7f70e51d4d82a0a517059d9f7da32d5ea513c
zigazl <zlogar.ziga@gmail.com>
2022-04-25 22:51:13 +0200

    Add files via upload

---
+++ twoStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Write a program to read two strings and report whether the strings are equal. If not, report
+//which of the two is the larger. Now, change the program to report whether the strings
+//have the same length and if not report which is longer.
+
+namespace twoStrings {
+    class Program {
+        static void Main(string[] args) {
+            
+            
+            Console.WriteLine("Enter string 1");
+            var str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter string 2");
+            var str2 = Console.ReadLine();
+
+            if (str1 == str2) {
+                Console.WriteLine("Strings are equal");
+            } else if (str1.Length == str2.Length){
+                Console.WriteLine("String sizes are the same");
+            }
+            else {
+                if(str1.Length > str2.Length) {
+                    Console.WriteLine("String 1 is larger");
+                }
+                else {
+                    Console.WriteLine("String 2 is larger");
+                }
+            }
+
+        }
+    }
+}
---
+++ vectorSum/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Read a set of integers into a vector. Calculate and print the sum of each pair of adjacent
+//elements in the vector. If there is an odd number, tell the user and print the value of the
+//last element without summing it. Now change your program so that it prints the sum of
+//the first and last elements, followed by the sum of the second and second-to-last and so
+//on.
+//A good replacement for C++ vector in C# is a list
+
+namespace vectorSum {
+    class Program {
+
+        static void SumAdjecent(int sum, List<int> numbers) {
+            for(var i = 0; i < numbers.Count; i += 2) {
+                sum = numbers[i] + numbers[i + 1];
+                Console.Write(sum + " ");
+            }
+        }
+
+        static void Main(string[] args) {
+            var numbers = new List<int>();
+            var input = 0;
+            var sum = 0;
+            while(Int32.TryParse(Console.ReadLine(), out input)) {
+                numbers.Add(input);
+            }
+
+
+            if(numbers.Count % 2 == 0) {
+                SumAdjecent(sum, numbers);
+                for(var i = 0; i < numbers.Count / 2; i++) {
+                    var num1 = numbers[i];
+                    var num2 = numbers[numbers.Count - 1 - i];
+                    sum = num1 + num2;
+                    Console.WriteLine(num1 + " + " + num2 + " = " + sum);
+                }
+
+            }
+            else {
+                Console.WriteLine("The number of elements is odd");
+                SumAdjecent(sum, numbers);
+                for(var i = 0; i <= numbers.Count / 2; i++) {
+                    var num1 = numbers[i];
+                    var num2 = numbers[numbers.Count - 1 - i];
+                    sum = num1 + num2;
+                    Console.WriteLine(num1 + " + " + num2 + " = " + sum);
+                    if(i == numbers.Count / 2) {
+                        Console.WriteLine("The middle number is: " + numbers[i + 1]);
+                    }
+                }
+                Console.WriteLine("The last number of the list is " + numbers[numbers.Count - 1]);
+
+            }
+        }
+
+    }
+}
+
